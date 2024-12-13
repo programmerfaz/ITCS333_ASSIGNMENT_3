a2a5ac7ccc6e65be75563d0a62808a55dc06c0a5
unknown <202103703@stu.uob.edu.bh>
2024-12-13 16:35:42 +0300

    updat

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getData() {
-    const url = "https://data.gov.bh/explore/embed/dataset/01-statistics-of-students-nationalities_updated/table/?disjunctive.year&disjunctive.semester&disjunctive.the_programs&sort=number_of_students";
+    const url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
     try {
         const response = await fetch(url);
         if (!response.ok) {
